e91d09a3c424af4625da805fe3a83178a9bd0209
Isaac Vieira <isaacvf.dev@gmail.com>
2023-10-19 13:31:01 -0300

    Working on contactpage, fixing submit requirements

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -4,29 +4,27 @@
       <div class="mb-3 col-7" style="padding: 0 78px">
         <label for="name" class="form-label">Name</label>
         <input v-model="form.name" type="text" class="form-control" id="name" placeholder="Ex.: Your full name">
-        <p v-for="(error, index) of v$.$errors" :key="index">
-          <strong>{{ error.$validator }}</strong>
-          <small> on property</small>
-          <strong>{{ error.$property }}</strong>
-          <small> says:</small>
-          <strong>{{ error.$message }}</strong>
-        </p>
+        <div v-if="v$.form.name.$error" style="color: #EC1D24;">Invalid name</div>
       </div>
       <div class="mb-3 col-7" style="padding: 0 78px">
         <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" placeholder="Ex.: myemail@email.com" required>
+        <input type="email" class="form-control" id="email" placeholder="Ex.: myemail@email.com">
+        <div v-if="!v$.form.email.$error" style="color: #EC1D24;">Email is required.</div>
+        <div v-if="!v$.form.email.email" style="color: #EC1D24;">Invalid email format.</div>
       </div>
       <div class="mb-3 col-7" style="padding: 0 78px">
         <label for="phone" class="form-label">Phone</label>
-        <input type="number" class="form-control" id="phone" placeholder="Ex.: (99) 9 9999-9999" required>
+        <input type="tel" class="form-control" id="phone" placeholder="Ex.: (99) 9 9999-9999">
+        <div v-if="!v$.form.phone.$error" style="color: #EC1D24;">Phone is required.</div>
+        <div v-if="!v$.form.phone.validPhone" style="color: #EC1D24;">Invalid phone format (Ex.: (99) 9 9999-9999).</div>
       </div>
       <div class="mb-3 col-7" style="padding: 0 78px">
         <label for="message" class="form-label">Message</label>
-        <textarea class="form-control" id="message" rows="3" placeholder="Ex.: Contact message for Marvel" required
+        <textarea class="form-control" id="message" rows="3" placeholder="Ex.: Contact message for Marvel"
           style="resize: none;"></textarea>
       </div>
       <div class="mb-3 col-7" style="margin: 0 68px">
-        <button @click="submit()" type="submit" class="btn btn-secondary">Send</button>
+        <button @click="submit()" type="button" class="btn btn-secondary">Send</button>
       </div>
 
     </div>
@@ -42,7 +40,8 @@ export default {
   data: () => ({
     form: {
       name: '',
-      email: ''
+      email: '',
+      phone: ''
     }
   }),
   setup: () => ({ v$: useVuelidate() }),
@@ -52,7 +51,7 @@ export default {
       console.log(result)
       if (!result) {
         // notify user form is invalid
-        return
+        return false
       }
       // perform async actions
     }
@@ -62,7 +61,13 @@ export default {
       form: {
         name: { required },
         email: { required, email },
-
+        phone: {
+          required,         
+          validPhone: (value) => {          
+          const phoneRegex = /^\(\d{2}\)\s\d\s\d{4}-\d{4}$/;
+          return phoneRegex.test(value);
+        }
+        }
       }
     }
   }
